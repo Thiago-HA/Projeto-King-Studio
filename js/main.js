8d1d62926c58224935a7f82b32e62781049f6ac0
Thiago <thiagopapadoce@outlook.com>
2022-08-10 21:03:34 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: false,
+    speed:700,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+});
+
+
